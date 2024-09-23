adee54c6b0a6b982a8da71348154e138036b30c6
Ana Carolina Orlando <anaorlandog@gmail.com>
2024-09-23 20:23:18 -0300

    new

--- exercicio6.py
+++ exercicio6.py
@@ -4,3 +4,25 @@
 #dragões diferentes e determine qual dragão é mais rápido, ou se ambos têm a mesma
 #velocidade.
 #b. Conceito: Operadores aritméticos, operadores relacionais, desvio condicional composto.
+
+print("------------------------------------------------------------------------------------------")
+
+n1 = float(input("Informe a distância que o do dragão 1 percorre "))
+
+n2 = float(input("Informe o tempo em que ele corre o dragão 1  "))
+
+n3 = float(input("Informe a distância que o do dragão 2 percorre "))
+
+n4 = float(input("Informe o tempo em que ele corre o dragão 2  "))
+
+t1= n1/n3
+t2 = n2/n4
+
+if t1>t2:
+    print("O dragão 1 é mais rápido")
+elif t2>t1:
+    print("O dragão 2 é mais rápido")
+else:
+    print("Os dois tem a mesma velocidade")
+
+print("------------------------------------------------------------------------------------------")
